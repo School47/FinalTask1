b4c0a90ed343b78ce5b52ad3046a62d26ca99cee
School47 <School47@gmail.com>
2023-04-30 04:09:28 +0500

    added program

--- Program.cs
+++ Program.cs
@@ -9,3 +9,41 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] CreateMyArray()
+{
+    string[] arr = new string[] {"Hello", "2", "world", ":-)"};
+    return arr;
+}
+
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)  Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+string[] MakeResultArray(string[] arr)
+{
+    string[] resArray = new string[arr.Length];
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) { resArray[counter++] = arr[i]; }
+    }
+    Array.Resize(ref resArray, counter); // убрать пустые элементы в конце массива
+    return resArray;
+}
+
+string[] myArray = CreateMyArray();
+Console.WriteLine("Исходный массив:");
+PrintArray(myArray);
+string[] resultArray = MakeResultArray(myArray);
+Console.WriteLine("Новый массив массив:");
+PrintArray(resultArray);
